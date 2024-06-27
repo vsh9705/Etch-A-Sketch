032cf2d328b1c16daba7f197083e6a5d960d8acf
Vasu Sharma <vsh.9705@gmail.com>
2024-06-28 04:50:53 +0530

    user input added for number of pixels

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const grid = document.querySelector("#grid");
 
-for(var i = 0 ; i < 16 ; i ++){
-    const grid_row = document.createElement("div");
-    grid.appendChild(grid_row);
-    for(var j = 0 ; j < 16 ; j ++){
-        const grid_col = document.createElement("div");
-        grid_col.classList.add("pixel");
-        grid_row.appendChild(grid_col);
+let pixelNumber = 16;
+
+function createGrid (pixelNumber){
+    grid.innerHTML = "";
+    for(var i = 0 ; i < pixelNumber ; i ++){
+        const grid_row = document.createElement("div");
+        grid.appendChild(grid_row);
+        for(var j = 0 ; j < pixelNumber ; j ++){
+            const grid_col = document.createElement("div");
+            grid_col.classList.add("pixel");
+            grid_row.appendChild(grid_col);
+        }
     }
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel => {
+        pixel.addEventListener("mouseover", () => {
+            pixel.style.backgroundColor = "blue";
+        })
+    })
 }
 
-const pixels = document.querySelectorAll(".pixel");
+createGrid(pixelNumber);
 
-pixels.forEach(pixel => {
-    pixel.addEventListener("mouseover", () => {
-        pixel.style.backgroundColor = "blue";
-    })
+const input = document.querySelector("#input");
+
+input.addEventListener("click", () => {
+    let userInput = prompt("Enter number of pixels");
+    if(userInput > 100){
+        alert("number less than 100!");
+        userInput = prompt("Enter number of pixels");
+    }
+    createGrid(userInput);
 })
