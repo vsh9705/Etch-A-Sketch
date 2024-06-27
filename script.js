c9b4e99ebc0b2a4ab3a19bb67cc0ca676fdee5e6
Vasu Sharma <vsh.9705@gmail.com>
2024-06-28 04:01:15 +0530

    16x16 grid made using js

---
+++ script.js
@@ -0,0 +1,11 @@
+const grid = document.querySelector("#grid");
+
+for(var i = 0 ; i < 16 ; i ++){
+    const grid_row = document.createElement("div");
+    grid.appendChild(grid_row);
+    for(var j = 0 ; j < 16 ; j ++){
+        const grid_col = document.createElement("div");
+        grid_col.classList.add("pixel");
+        grid_row.appendChild(grid_col);
+    }
+}
